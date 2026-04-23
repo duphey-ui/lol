609f2e4dc7b842b7e965667903d1fa97cae432b3
duphey-ui <duphey@gmail.com>
2026-04-23 13:05:59 +0300

    Add files via upload

---
+++ floyad.py
@@ -0,0 +1,9 @@
+rows = int(input("pease enter the total number of rows"))
+number = 1
+
+print("triangles")
+
+for i in range(1, rows+1):
+
+    for j in range(1, i+1):
+        print(number,end=' ')
