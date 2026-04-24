20b187e89480bc0376921ec56ac9a4bafaf4b2af
duphey-ui <duphey@gmail.com>
2026-04-24 16:34:15 +0300

    Add files via upload

---
+++ les6/castle.py
@@ -0,0 +1,32 @@
+import turtle #importing library
+
+turtle.Screen().bgcolor("orange")
+
+pen =turtle.Turtle()
+
+pen.forward(100)
+
+pen.left(120)
+pen. forward(100)
+
+pen.left(120)
+pen. forward(100)
+
+pen.penup()
+pen.right(150)
+pen.forward(50)
+
+pen.pendown()
+pen.right(90)
+pen. forward(100)
+
+pen.right(120)
+pen. forward(100)
+
+pen.right(120)
+pen. forward(100)
+
+
+turtle.done()
+
+
