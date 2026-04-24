ac989d2c281407129c260f6055c0f925364a8641
duphey-ui <duphey@gmail.com>
2026-04-24 21:11:05 +0300

    Add files via upload

---
+++ l/clip.py
@@ -0,0 +1,19 @@
+import turtle #importing library
+
+turtle.Screen().bgcolor("orange")
+
+pen =turtle.Turtle()
+
+pen.right(90)
+pen. forward(100)
+
+pen.right(90)
+pen. forward(100)
+
+pen.right(90)
+pen. forward(100)
+
+pen.right(90)
+pen. forward(100) 
+
+turtle.done()
